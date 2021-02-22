66d7fd0aa3691d37fc69f7f85663a3454d45e4dc
Alva Yonara Puramandya <alvayonara@outlook.com>
2021-02-22 21:07:44 +0700

    Initial commit

---
+++ Manga Collection/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Manga Collection
+//
+//  Created by Alva Yonara Puramandya on 22/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Manga Collection/Manga_CollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Manga_CollectionApp.swift
+//  Manga Collection
+//
+//  Created by Alva Yonara Puramandya on 22/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Manga_CollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
